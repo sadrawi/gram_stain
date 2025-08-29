7d4596f76e0b19662e0f010b49f824a4b6c295c7
Sadrawi <dr.sadrawi@gmail.com>
2025-08-29 16:58:04 +0700

    Update app_start.py

--- app_start.py
+++ app_start.py
@@ -9,6 +9,17 @@ import cv2
 import numpy as np
 import streamlit.components.v1 as components
 
+import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
+from ultralytics import YOLO
+import tempfile
+import os
+import requests
+# Load YOLOv8 segmentation model
+
+
 st.set_page_config(
     page_title="i3L AI System",
     layout="wide",
@@ -26,8 +37,18 @@ with col2:
 
 classes = ["Bacilli_N", "Bacilli_P", "Cocci_N", "Cocci_P", "Fungus"]
 
-covid_model_path = Path('best_gramstain.pt')
-covid_model = YOLO(covid_model_path)
+model_path = "best_gramstain.pt"
+
+if not os.path.exists(model_path):
+    url = "https://huggingface.co/Sadrawi/gram_stain/resolve/main/best_gramstain.pt"
+    with open(model_path, 'wb') as f:
+        f.write(requests.get(url).content)
+
+model = YOLO(model_path)
+
+
+# covid_model_path = Path('best_gramstain.pt')
+# covid_model = YOLO(covid_model_path)
 
 col1, col2 = st.columns(2)
 
@@ -39,9 +60,10 @@ with col1:
 with col2:
     if st.button('Detect'):
         source_img = PIL.Image.open(source_img)
-        res = covid_model.predict(source_img)
+        res = model.predict(source_img)
         # boxes = res[0].boxes
         res_plotted = res[0].plot()[:, :, ::-1]
         st.image(res_plotted, 
             caption='Detected Image',
             use_container_width =True  )
+
