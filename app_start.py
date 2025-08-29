a66fea1c2623997b96ec1063a5824fe33000e7d9
Sadrawi <dr.sadrawi@gmail.com>
2025-08-29 17:00:29 +0700

    Update app_start.py

--- app_start.py
+++ app_start.py
@@ -1,12 +1,12 @@
 # https://github.com/CodingMantras/yolov8-streamlit-detection-tracking
 
 from pathlib import Path
-import PIL
-from ultralytics import YOLO
-from PIL import Image
-import streamlit as st
-import cv2
-import numpy as np
+# import PIL
+# from ultralytics import YOLO
+# from PIL import Image
+# import streamlit as st
+# import cv2
+# import numpy as np
 import streamlit.components.v1 as components
 
 import streamlit as st
@@ -66,4 +66,5 @@ with col2:
         st.image(res_plotted, 
             caption='Detected Image',
             use_container_width =True  )
+
 
